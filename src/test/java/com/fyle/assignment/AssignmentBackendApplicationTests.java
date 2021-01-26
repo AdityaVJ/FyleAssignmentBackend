8e466b24e44a65232bb8c53ffe996ec202f7cbb6
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2021-01-26 14:04:13 +0530

    Initial folder structure ready.

---
+++ src/test/java/com/fyle/assignment/AssignmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyle.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
